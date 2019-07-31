6054433d547ee6ca86362d1671d743c3c7a4110d
Caio Luan <caioluanc@hotmail.com>
2019-07-31 16:54:28 -0300

    Create transform CGISize

--- build.js
+++ build.js
@@ -1,7 +1,12 @@
 const StyleDictionary = require('style-dictionary');
+const _ = require('lodash');
 
 function isOpacityGroup(prop) {
   return prop.group === 'opacity';
+}
+
+function isOffset(prop) {
+  return prop.attributes.type === 'offset';
 }
 
 console.log('Build started...');
@@ -16,14 +21,37 @@ StyleDictionary.registerTransform({
   }
 });
 
+StyleDictionary.registerTransform({
+  name: 'color/cgsize',
+  type: 'value',
+  matcher: isOffset,
+  transformer: function(prop) {
+    const value = prop.value.split(' ');
+    return `CGSize(width: ${value[0]}, height:${value[1]})`;
+  }
+});
+
+StyleDictionary.registerTransform({
+  name: 'name/ti/camel',
+  type: 'name',
+  transformer: function(prop, options) {
+    return _.camelCase([options.prefix].concat(prop.path).join(' '));
+  }
+});
+
 StyleDictionary.registerTransformGroup({
   name: 'custom/web',
   transforms: ["attribute/cti", "name/cti/kebab", 'opacity/number']
 });
 
+StyleDictionary.registerTransformGroup({
+  name: 'custom/ios',
+  transforms: ["attribute/cti", "name/ti/camel", "color/UIColorSwift", "color/cgsize"]
+});
+
 StyleDictionaryExtended = StyleDictionary.extend(__dirname + '/config.json');
 
-StyleDictionaryExtended.buildPlatform('web');
+StyleDictionaryExtended.buildPlatform("ios-swift");
 
 console.log('\n==============================================');
 console.log('\nBuild completed!');
