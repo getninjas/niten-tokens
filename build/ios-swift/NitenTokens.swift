74d90629741a1d0b14850c6c5c1a324609b672aa
Caio Luan <caioluanc@hotmail.com>
2019-07-31 16:55:50 -0300

    Create first build ios platform

---
+++ build/ios-swift/NitenTokens.swift
@@ -0,0 +1,105 @@
+
+//
+// NitenTokens.swift
+//
+// Do not edit directly
+// Generated on Wed, 31 Jul 2019 19:46:11 GMT
+//
+
+
+import UIKit
+
+public class NitenTokens {
+    static let borderStyleDefault = solid
+    static let colorBrandFirstDark = UIColor(red: 0.13, green: 0.13, blue: 0.13, alpha:1)
+    static let colorBrandFirstExtraDark = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha:1)
+    static let colorBrandFirstLight = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha:1)
+    static let colorBrandFirstMedium = UIColor(red: 0.34, green: 0.34, blue: 0.34, alpha:1)
+    static let colorBrandSecondDark = UIColor(red: 1.00, green: 0.92, blue: 0.32, alpha:1)
+    static let colorBrandSecondExtraDark = UIColor(red: 0.38, green: 0.34, blue: 0.00, alpha:1)
+    static let colorBrandSecondLight = UIColor(red: 1.00, green: 0.99, blue: 0.90, alpha:1)
+    static let colorBrandSecondMedium = UIColor(red: 1.00, green: 0.95, blue: 0.60, alpha:1)
+    static let colorInterfaceActionDark = UIColor(red: 0.00, green: 0.62, blue: 0.93, alpha:1)
+    static let colorInterfaceActionExtraDark = UIColor(red: 0.10, green: 0.36, blue: 0.66, alpha:1)
+    static let colorInterfaceActionLight = UIColor(red: 0.90, green: 0.97, blue: 1.00, alpha:1)
+    static let colorInterfaceActionMedium = UIColor(red: 0.48, green: 0.84, blue: 1.00, alpha:1)
+    static let colorInterfaceAlertDark = UIColor(red: 0.96, green: 0.73, blue: 0.31, alpha:1)
+    static let colorInterfaceAlertExtraDark = UIColor(red: 0.84, green: 0.56, blue: 0.00, alpha:1)
+    static let colorInterfaceAlertLight = UIColor(red: 1.00, green: 0.99, blue: 0.90, alpha:1)
+    static let colorInterfaceAlertMedium = UIColor(red: 1.00, green: 0.94, blue: 0.52, alpha:1)
+    static let colorInterfaceContentDark = UIColor(red: 0.13, green: 0.13, blue: 0.13, alpha:1)
+    static let colorInterfaceContentExtraDark = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha:1)
+    static let colorInterfaceContentLight = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha:1)
+    static let colorInterfaceContentMedium = UIColor(red: 0.34, green: 0.34, blue: 0.34, alpha:1)
+    static let colorInterfaceHighlightDark = UIColor(red: 0.60, green: 0.39, blue: 0.93, alpha:1)
+    static let colorInterfaceHighlightExtraDark = UIColor(red: 0.33, green: 0.10, blue: 0.66, alpha:1)
+    static let colorInterfaceHighlightLight = UIColor(red: 0.95, green: 0.92, blue: 1.00, alpha:1)
+    static let colorInterfaceHighlightMedium = UIColor(red: 0.78, green: 0.64, blue: 1.00, alpha:1)
+    static let colorInterfaceNegativeDark = UIColor(red: 0.95, green: 0.40, blue: 0.40, alpha:1)
+    static let colorInterfaceNegativeExtraDark = UIColor(red: 0.71, green: 0.01, blue: 0.00, alpha:1)
+    static let colorInterfaceNegativeLight = UIColor(red: 0.99, green: 0.93, blue: 0.93, alpha:1)
+    static let colorInterfaceNegativeMedium = UIColor(red: 0.96, green: 0.60, blue: 0.60, alpha:1)
+    static let colorInterfacePositiveDark = UIColor(red: 0.15, green: 0.63, blue: 0.35, alpha:1)
+    static let colorInterfacePositiveExtraDark = UIColor(red: 0.00, green: 0.40, blue: 0.07, alpha:1)
+    static let colorInterfacePositiveLight = UIColor(red: 0.92, green: 0.98, blue: 0.95, alpha:1)
+    static let colorInterfacePositiveMedium = UIColor(red: 0.63, green: 0.91, blue: 0.75, alpha:1)
+    static let colorInterfaceShadeDark = UIColor(red: 0.86, green: 0.86, blue: 0.86, alpha:1)
+    static let colorInterfaceShadeExtraDark = UIColor(red: 0.70, green: 0.70, blue: 0.70, alpha:1)
+    static let colorInterfaceShadeLight = UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha:1)
+    static let colorInterfaceShadeMedium = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha:1)
+    static let colorOpacityOpaque = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha:1)
+    static let colorOpacityTranslucent = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha:1)
+    static let fontFamilyDefault = 'Source Sans Pro', Helvetica, Arial, sans-serif
+    static let fontStyleQuote = italic
+    static let fontStyleWeightBold = 600
+    static let fontStyleWeightLight = 300
+    static let fontStyleWeightRegular = 400
+    static let shadowBottomLevel1 = 0 2px 4px 0 rgba(0, 0, 0, .15)
+    static let shadowBottomLevel2 = 0 4px 12px 0 rgba(0, 0, 0, .12)
+    static let shadowBottomLevel3 = 0 12px 24px 0 rgba(0, 0, 0, .1)
+    static let shadowOffsetBottom1 = CGSize(width: 0, height:2)
+    static let shadowOffsetBottom2 = CGSize(width: 0, height:4)
+    static let shadowOffsetBottom3 = CGSize(width: 0, height:12)
+    static let shadowOffsetTop1 = CGSize(width: 0, height:-2)
+    static let shadowOffsetTop2 = CGSize(width: 0, height:-4)
+    static let shadowOffsetTop3 = CGSize(width: 0, height:-12)
+    static let shadowRadius1 = 4
+    static let shadowRadius2 = 12
+    static let shadowRadius3 = 24
+    static let shadowTopLevel1 = 0 -2px 4px 0 rgba(0, 0, 0, .15)
+    static let shadowTopLevel2 = 0 -4px 12px 0 rgba(0, 0, 0, .12)
+    static let shadowTopLevel3 = 0 -12px 24px 0 rgba(0, 0, 0, .1)
+    static let sizeBorderRadiusCircle = 50%
+    static let sizeBorderRadiusL = 16px
+    static let sizeBorderRadiusM = 8px
+    static let sizeBorderRadiusNone = 0
+    static let sizeBorderRadiusPill = 160px
+    static let sizeBorderRadiusS = 4px
+    static let sizeBorderWidthNone = 0
+    static let sizeBorderWidthThick = 2px
+    static let sizeBorderWidthThin = 1px
+    static let sizeFontL = 32px
+    static let sizeFontM = 24px
+    static let sizeFontS = 20px
+    static let sizeFontXl = 48px
+    static let sizeFontXs = 16px
+    static let sizeFontXxl = 60px
+    static let sizeFontXxs = 14px
+    static let sizeLineHeightEqual = 1
+    static let sizeLineHeightL = 40px
+    static let sizeLineHeightM = 32px
+    static let sizeLineHeightS = 28px
+    static let sizeLineHeightXl = 56px
+    static let sizeLineHeightXs = 24px
+    static let sizeLineHeightXxl = 68px
+    static let sizeLineHeightXxs = 22px
+    static let sizeSpacingL = 48px
+    static let sizeSpacingM = 36px
+    static let sizeSpacingS = 24px
+    static let sizeSpacingXl = 60px
+    static let sizeSpacingXs = 16px
+    static let sizeSpacingXxl = 84px
+    static let sizeSpacingXxs = 12px
+    static let sizeSpacingXxxl = 148px
+    static let sizeSpacingXxxs = 8px
+}
