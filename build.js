ae28a4d833046256ffbbd13a66de09c26b16d247
williamcosta <dev.williamcosta@gmail.com>
2019-07-31 11:28:44 -0300

    Remove comments to build file

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ console.log('Build started...');
 console.log('\n==============================================');
 
 StyleDictionary.registerTransform({
-  name: 'opacity/number', // notice: the name is an override of an existing predefined method (yes, you can do it)
+  name: 'opacity/number',
   type: 'value',
   matcher: isOpacityGroup,
   transformer: function(prop) {
@@ -18,18 +18,11 @@ StyleDictionary.registerTransform({
 
 StyleDictionary.registerTransformGroup({
   name: 'custom/web',
-  // this is just to show a possibility, if you want to add a few transforms to a pre-defined group
-  // (even so, we suggest to use the previous approach, which is more explicit and clear)
   transforms: ["attribute/cti", "name/cti/kebab", 'opacity/number']
 });
 
-// APPLY THE CONFIGURATION
-// Very important: the registration of custom transforms
-// needs to be done _before_ applying the configuration
 StyleDictionaryExtended = StyleDictionary.extend(__dirname + '/config.json');
 
-
-// FINALLY, BUILD ALL THE PLATFORMS
 StyleDictionaryExtended.buildPlatform('web');
 
 console.log('\n==============================================');
